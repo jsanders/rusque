bd78c81e047f2072ec72d405f04b1d8378fdf241
James Sanders <sanderjd@gmail.com>
2013-10-01 17:10:05 -0600

    Implement lpush, mostly just to write a reasonable lpop test
    But also forced out integer reply

--- src/redis.rs
+++ src/redis.rs
@@ -23,6 +23,11 @@ struct Redis {
   stream: TcpStream
 }
 
+enum Reply {
+  BulkReply(Option<~str>),
+  IntegerReply(int)
+}
+
 impl Redis {
   fn connect() -> Redis {
     let ip_addr = Ipv4Addr(127, 0, 0, 1);
@@ -43,52 +48,66 @@ impl Redis {
     }
   }
 
-  fn read_reply(&mut self) -> Option<~str> {
+  fn read_reply(&mut self) -> Reply {
     let mut buf = ~[0u8, ..1024];
     let bytes_read = self.stream.read(buf).expect("Failed to get reply from redis server");
     let reply = str::from_utf8(buf.slice_to(bytes_read));
-    let (len_line, rest) = match reply.find_str("\r\n") {
+    let (header, rest) = match reply.find_str("\r\n") {
       None => fail!("Invalid reply"),
       Some(index) => {
         ( reply.slice_to(index), reply.slice_from(index + 2) )
       }
     };
 
-    match len_line[0] as char {
-      '$' => self.read_bulk_reply(len_line.slice_from(1), rest),
+    let (sentinel, header_data) = (header[0] as char, header.slice_from(1));
+    match sentinel {
+      '$' => self.read_bulk_reply(header_data, rest),
+      ':' => self.read_integer_reply(header_data, rest),
       _   => fail!("Invalid reply")
     }
   }
 
-  fn read_bulk_reply(&mut self, len: &str, rest: &str) -> Option<~str> {
+  fn read_bulk_reply(&self, len: &str, rest: &str) -> Reply {
     let len: int = from_str(len).expect("Invalid bulk reply");
-    match len {
+    BulkReply(match len {
       -1 => None,
       _  => {
         let len = len.to_uint_safe().expect("Invalid bulk reply");
         Some(rest.slice_to(len).to_owned())
       }
-    }
+    })
   }
 
-  fn do_command(&mut self, args: &[&str]) -> Option<~str> {
+  fn read_integer_reply(&self, integer: &str, rest: &str) -> Reply {
+    assert!(rest.len() == 0, "Invalid integer reply");
+
+    IntegerReply(from_str::<int>(integer).expect("Invalid integer reply"))
+  }
+
+  fn do_command(&mut self, args: &[&str]) -> Reply {
     self.write_command(args);
     self.read_reply()
   }
 
   fn lpop(&mut self, key: &str) -> Option<~str> {
-    self.do_command(&[ &"lpop", key ])
+    match self.do_command(&[ &"lpop", key ]) {
+      BulkReply(reply) => reply,
+      _ => fail!("Protocol error: `lpop` expects bulk reply")
+    }
+  }
+
+  fn lpush(&mut self, key: &str, value: &str) -> int {
+    match self.do_command(&[ &"lpush", key, value ]) {
+      IntegerReply(reply) => reply,
+      _ => fail!("Protocol error: `lpush` expects integer reply")
+    }
   }
 }
 
 #[test]
-fn test_connect() {
+fn test_lpop_lpush() {
   let mut redis = Redis::connect();
-  println(
-    match redis.lpop("some_key") {
-      Some(value) => value,
-      None        => ~"Nothing to pop!"
-    }
-  );
+  redis.lpush("some_key", "some_value");
+  let popped = redis.lpop("some_key").expect("Failed to pop!");
+  assert_eq!(popped, ~"some_value")
 }
-
